174c42a79c2b2c34092543fcd2dffd325a3b181b
Hakan Yalniz <hakan_yalniz@live.com>
2023-08-06 16:29:56 +0300

    Added various references to the buttons and their event listeners. Added removeElementFromArray, though right now it is unused, since I found a better method. Added turnArrayToNumber, which turns arrays made of string into number, uses !isNaN to check whether they can be turned into a number. Added combineNumbers, which combines the strings within an array into one, it uses an element that can't be turned into a number as the seperator, which in this case is the operators. Changed how the operators work, now they make use of combineNumbers and turnArrayToNumber.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 function add(numberArgs) {
   let result = 0;
+  numberArgs = combineNumbers(numberArgs);
+  numberArgs = turnArrayToNumber(numberArgs);
+
   for (let i = 0; i < numberArgs.length; i++) {
     if (numberArgs[i + 1] == undefined) {
       break;
@@ -11,6 +14,9 @@ function add(numberArgs) {
 
 function subtract(numberArgs) {
   let result = 0;
+  numberArgs = combineNumbers(numberArgs);
+  numberArgs = turnArrayToNumber(numberArgs);
+
   for (let i = 0; i < numberArgs.length; i++) {
     if (numberArgs[i + 1] == undefined) {
       break;
@@ -23,6 +29,9 @@ function subtract(numberArgs) {
 
 function multiply(numberArgs) {
   let result = 0;
+  numberArgs = combineNumbers(numberArgs);
+  numberArgs = turnArrayToNumber(numberArgs);
+
   for (let i = 0; i < numberArgs.length; i++) {
     if (numberArgs[i + 1] == undefined) {
       break;
@@ -35,6 +44,9 @@ function multiply(numberArgs) {
 
 function divide(numberArgs) {
   let result = 0;
+  numberArgs = combineNumbers(numberArgs);
+  numberArgs = turnArrayToNumber(numberArgs);
+
   for (let i = 0; i < numberArgs.length; i++) {
     if (numberArgs[i + 1] == undefined) {
       break;
@@ -45,24 +57,72 @@ function divide(numberArgs) {
   return result;
 }
 
-function operate(...theArgs) {
+function operate(theArgs) {
+  // Checks which operation to do then calls the proper operator functions with the whole array
+  // Also removes operator sign from the array
   for (const element of theArgs) {
     if (element == "+") {
-      theArgs.pop();
+      //   removeElementFromArray(theArgs, "+");
+
       return add(theArgs);
     } else if (element == "-") {
-      theArgs.pop();
+      //   removeElementFromArray(theArgs, "-");
+
       return subtract(theArgs);
     } else if (element == "*") {
-      theArgs.pop();
+      //   removeElementFromArray(theArgs, "*");
+
       return multiply(theArgs);
     } else if (element == "/") {
-      theArgs.pop();
+      //   removeElementFromArray(theArgs, "/");
+
       return divide(theArgs);
     }
   }
 }
 
+function turnArrayToNumber(theArray) {
+  // Converts the array into numbers, but only the ones that can be converted
+  // !isNaN checks if the array element can be turned into number of not
+  for (let x = 0; x < theArray.length; x++) {
+    if (typeof theArray[x] == "string" && !isNaN(theArray[x])) {
+      theArray[x] = Number(theArray[x]);
+    }
+  }
+
+  return theArray;
+}
+
+function combineNumbers(theArray) {
+  let combined = "";
+  let newArray;
+  // Combines all the array elements into combined
+  for (let x = 0; x < theArray.length; x++) {
+    combined += theArray[x];
+  }
+  // Turns combined into an array, split only one parts which can't be numbers
+  // which happened to be operators
+  // This would mean that each right and left part of the operators will be combined
+  for (const element of theArray) {
+    if (isNaN(element)) {
+      newArray = combined.split(element);
+    }
+  }
+
+  return newArray;
+}
+
+// Array pop was used before, but that only removed the last item
+// This finds the element and removes it, no matter where it is
+function removeElementFromArray(theArray, remove) {
+  const index = theArray.indexOf(remove);
+  if (index != -1) {
+    theArray.splice(index, 1);
+  }
+}
+
+// displayInput.innerHTML is the display on the HTML
+// element.innerHTML is the innerHTML of things such as buttons
 function writeDisplay(element) {
   currentDisplay += element.innerHTML;
   displayInput.innerHTML = currentDisplay;
@@ -72,13 +132,23 @@ function writeDisplay(element) {
 let displayInput = document.getElementById("display-input");
 // The current display, things will get written into it before they are written into the screen
 let currentDisplay = "";
+let numbersInMemory = [];
 
 // References to the buttons and their event listeners
 
 let buttons = document.getElementById("buttons");
+let equalsButton = document.getElementById("equals");
 
+// The clicked button will call writeDisplay to display it on screen and also will store it in memory
+// so that we can operate on it
 buttons.addEventListener("click", function (event) {
-  if (event.target.type == "button") {
+  if (event.target.type == "button" && event.target.innerHTML != "=") {
     writeDisplay(event.target);
+    numbersInMemory.push(event.target.innerHTML);
   }
+});
+
+// Clicking the equal will send the numbers in memory to operate
+equalsButton.addEventListener("click", function (event) {
+  console.log(operate(numbersInMemory));
 });
